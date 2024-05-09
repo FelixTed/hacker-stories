77b3ad2de7ae8ab0757d65c4cb157d24565641b1
Felix Cadieux <cadieuxfelix@outlook.com>
2024-05-09 13:04:18 -0400

    up to page 27

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import * as React from 'react';
 
-const title = 'React';
+const list = [
+  {
+    title: "React",
+    url: 'https://reactjs.org',
+    author: "Jordan Walke",
+    num_comment:3,
+    points: 4,
+    objectID:3,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author:"Dan Abramov, Andrew Clarke",
+    num_comments: 3,
+    points: 4,
+    objectID:1,
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        welcome to the web
+      </h1>
 
-      <label htmlFor="search">Search:</label>
+      <label htmlFor="search">Search:</label><br></br>
       <input id="search" type="text"/>
+
+      <hr />
+      {/*render list here */}
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comment}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
+      
     </div>
   );
 }
