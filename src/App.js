1eb332aad9b068484f73e85d81ac1935c383a568
Felix Cadieux <cadieuxfelix@outlook.com>
2024-05-11 10:17:40 -0400

    up to page 51

--- src/App.js
+++ src/App.js
@@ -20,13 +20,17 @@ const App = () => {
       objectID:1,
     }
   ];
+const handleSearch = (event) => {
+  console.log(event.target.value);
+};
+
   return(
     <div>
       <h1>
         welcome to my first react app
       </h1>
 
-      <Search/>
+      <Search onSearch={handleSearch}/>
 
       <hr />
       
@@ -37,7 +41,8 @@ const App = () => {
   );
 }
 
-const List = (props) => (
+const List = (props) =>{
+  return(
     <ul>
         {props.list.map((item) =>  (
             <Item key = {item.objectID} item = {item}/>
@@ -45,8 +50,10 @@ const List = (props) => (
         )}
       </ul>
   );
+}
 
-  const Item = (props) => (
+  const Item = (props) =>{
+    return(
     <li>
       <span>
       <a href={props.item.url}>{props.item.title}</a>
@@ -56,15 +63,24 @@ const List = (props) => (
       <span>{props.item.points}</span>
     </li>
   );
+}
   
-const Search = () =>{
+const Search = (props) =>{
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = (event) => {
-    console.log(event.target.value);
-  }
+    setSearchTerm(event.target.value);
+
+    props.onSearch(event);
+  };
   return(
     <div>
-    <label htmlFor="search">Search:</label><br></br>
+    <label htmlFor="search">Search:</label>
       <input id="search" type="text" onChange={handleChange}/>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   );
 }
