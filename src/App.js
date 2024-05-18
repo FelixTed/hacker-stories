93b84ace57fd5b0c1e5e6a7116d97bb2fc6c3055
Felix Cadieux <cadieuxfelix@outlook.com>
2024-05-17 23:12:36 -0400

    added space between each data field of a list element, for improved readability

--- src/App.js
+++ src/App.js
@@ -140,10 +140,10 @@ const List = ({list,onRemoveItem}) =>{
     <li>
       <span>
       <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
+      </span>&nbsp;
+      <span>{item.author}</span>&nbsp;
+      <span>{item.num_comments}</span>&nbsp;
+      <span>{item.points}</span>&nbsp;
       <span>
         <button type = "button" onClick={() => onRemoveItem(item)}>Dismiss</button>
       </span>
