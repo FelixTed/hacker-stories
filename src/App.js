b60d8966284c0eada38f59131ab01afd44e91d6c
Felix Cadieux <cadieuxfelix@outlook.com>
2024-05-21 19:05:29 -0400

    page 154

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import axios from "axios";
-import './App.scss';
+//import styles from './App.module.css';
+import styled from 'styled-components';
 
 
   const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query="
@@ -43,6 +44,82 @@ const useSemiPersistentState = (key,initialState) => {
 
   return [value,setValue];
 }
+
+const StyledContainer = styled.div`
+
+  height: 100vw;
+  padding; 20px;
+
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight:300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+display: flex;
+align-items: center;
+padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover{
+    backgroud: #171212;
+    color: #ffffff;
+  }
+`;
+// const StyledButtonSmall = styled(StyledButton)`
+//   padding: 5px;
+// `;
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20 px 0;
+  dispaly: flex;
+  align-items: baseline;
+`;
+const StyledLabel = styled.label`
+  border-top: 10px solid #171212;
+  border-left: 10px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+const StyledInput =styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
 
 const App = () => {
 
@@ -93,10 +170,10 @@ const App = () => {
   };
   
   return(
-    <div className="container">
-      <h1 className="headline-primary">
+    <StyledContainer>
+      <StyledHeadlinePrimary>
         welcome to my first react app
-      </h1>
+      </StyledHeadlinePrimary>
       
       <SearchForm searchTerm = {searchTerm} onSearchInput = {handleSearchInput} onSearchSubmit = {handleSearchSubmit}></SearchForm>
 
@@ -111,7 +188,7 @@ const App = () => {
       )}
       
 
-    </div>
+    </StyledContainer>
   );
 }
 
@@ -132,17 +209,17 @@ const List = ({list,onRemoveItem}) =>{
   const Item = ({item,onRemoveItem}) =>{
 
     return(
-    <li className='item'>
-      <span style={{width:'40%'}}>
+    <StyledItem>
+      <StyledColumn width = '40%'>
       <a href={item.url}>{item.title}</a>
-      </span>&nbsp;
-      <span style={{width:"30%"}}>{item.author}</span>&nbsp;
-      <span style ={{width:"10%"}}>{item.num_comments}</span>&nbsp;
-      <span style = {{width:"10%"}}>{item.points}</span>&nbsp;
-      <span style = {{width:"10%"}}>
-        <button type = "button" onClick={() => onRemoveItem(item)} className="button button_small">Dismiss</button>
-      </span>
-    </li>
+      </StyledColumn>
+      <StyledColumn width ="30%">{item.author}</StyledColumn>
+      <StyledColumn width = "10%">{item.num_comments}</StyledColumn>
+      <StyledColumn width ="10%">{item.points}</StyledColumn>
+      <StyledColumn width = "10%">
+        <StyledButton type = "button" onClick={() => onRemoveItem(item)} >Dismiss</StyledButton>
+      </StyledColumn>
+    </StyledItem>
   );
 }
   
@@ -157,24 +234,24 @@ const InputWithLabel = ({id,type, label, value, isFocused, onInputChange,childre
   
   return(
     <>
-    <label htmlFor={id} className ="label">{children}</label>
+    <StyledLabel htmlFor={id}>{children}</StyledLabel>
     &nbsp;
-      <input ref = {inputRef} id={id} type={type} value = {value} autoFocus={isFocused} onChange={onInputChange} className='input'/>
+      <StyledInput ref = {inputRef} id={id} type={type} value = {value} autoFocus={isFocused} onChange={onInputChange}/>
 
     </>
   )}
 
   const SearchForm = ({searchTerm,onSearchInput,onSearchSubmit}) =>(
-    <form onSubmit={onSearchSubmit} className="search-form">
+    <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel id = "search" value = {searchTerm} isFocused onInputChange = {onSearchInput}>
       <strong>Search:</strong>
     </InputWithLabel>
     
 
-    <button type="submit" disabled ={!searchTerm} className="button_large">
+    <StyledButtonLarge type="submit" disabled ={!searchTerm}>
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
   );
 
 
